eaf52effcc3056494230db1f8cb790a45acc976b
avijit mohajon <avijitmohajon72@gmail.com>
2025-02-23 12:20:45 +0600

    Finishing touch

--- src/Pages/ServiceDetails.jsx
+++ src/Pages/ServiceDetails.jsx
@@ -149,7 +149,7 @@ const ServiceDetails = () => {
               />
             </div>
 
-            {/* Submit Button */}
+            
             <button
               type="submit"
               className="mt-4 px-6 py-2 bg-green-500 text-white rounded-lg hover:bg-green-700 transition"
